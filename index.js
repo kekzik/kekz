711f981c9f93e561344eb1ec85cf7df0097dec71
Дмитрий Кузнецов <kekzik123@gmail.com>
2018-06-06 15:32:04 +0400

    выфв

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const token = '525299598:AAFSIRKqKAT19-ZJfjOU6HRkS-_sTFYgCVI'; // токен
-const TelegramBot = require('node-telegram-bot-api'); // библиотека
-
-// Включить опрос сервера
-const bot = new TelegramBot(token, {polling: true});
-
-// Простая команда без параметров.
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
-    // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
-    var photo = 'cats.png';
-    bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
+var TelegramBot = require('node-telegram-bot-api');
+var token = '525299598:AAFSIRKqKAT19-ZJfjOU6HRkS-_sTFYgCVI';
+var bot = new TelegramBot(token, {polling: true});
+var notes = [];
+bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+    notes.push({'uid': userId, 'time': time, 'text': text});
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
 });
+setInterval(function () {
+    for (var i = 0; i < notes.length; i++) {
+        var curDate = new Date().getHours() + ':' + new Date().getMinutes();
+        if (notes[i]['time'] == curDate) {
+            bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: ' + notes[i]['text'] + ' сейчас.');
+            notes.splice(i, 1);
+        }
+    }
+}, 1000);
