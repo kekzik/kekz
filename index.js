6f45d7b1bc8b604a18ad5bc2752b42a4c6e94779
Дмитрий Кузнецов <kekzik123@gmail.com>
2018-06-06 15:44:00 +0400

    выф

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '525299598:AAFSIRKqKAT19-ZJfjOU6HRkS-_sTFYgCVI';
 var bot = new TelegramBot(token, {polling: true});
 var notes = [];
-bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
+bot.onText(/\/start/, function (msg, match) {
     var userId = msg.from.id;
     var text = match[1];
     var time = match[2];
     notes.push({'uid': userId, 'time': time, 'text': text});
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :1111)');
+    bot.sendMessage(userId, 'Привет');
 });
 setInterval(function () {
     for (var i = 0; i < notes.length; i++) {
         var curDate = new Date().getHours() + ':' + new Date().getMinutes();
-        if (notes[i]['time'] == curDate) {
+        if (curDate == curDate) {
             bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: ' + notes[i]['text'] + ' сейчас.');
             notes.splice(i, 1);
         }
